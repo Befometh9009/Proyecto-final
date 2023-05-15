0f4a457ea8a19c4c7b4ad3c5d2022b9e36e2a056
Befometh9009 <crissm3932@gmail.com>
2023-05-15 20:33:58 +0100

    generación de atributos automáticos 15-05-23

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-
+const mainAttr = document.querySelector("#mainAttr")
+var vamp1;
 
 class Character {
     constructor() {
@@ -7,12 +8,12 @@ class Character {
             name: ""
         }
         this.attributes = {
-            // strength, dexterity, stamina
-            str: 1, dex: 1, sta: 1,
-            // charisma, manipulate, appearence
-            cha: 1, man: 1, app: 1,
-            // perception, intelligence, witness
-            per: 1, int: 1, wit: 1
+            // strength, charisma, perception
+            str: {id:0,num:1,spec:false}, cha: {id:1,num:1,spec:false}, per: {id:2,num:1,spec:false},
+            // dexterity, manipulate, intelligence
+            dex: {id:3,num:1,spec:false}, man: {id:4,num:1,spec:false}, int: {id:5,num:1,spec:false},
+            // stamina, appearence, witness
+            sta: {id:6,num:1,spec:false}, app: {id:7,num:1,spec:false}, wit: {id:8,num:1,spec:false}
         }
         this.habilities = {
             //empty until PC is created
@@ -24,8 +25,29 @@ class Character {
             //empty until PC is created
         }
 
-        this.willpower = [0, 0]
+        this.willpower = {
+            perm:0,
+            temp:0
+        }
         this.health = []
+    }
+    getAttrByID = id => {
+        let obj;
+        let attr = Object.getOwnPropertyNames(this.attributes); // crea un array con el nombre de todas las propiedades
+        for(let i=0; i<attr.length;i++){
+            let elem = Object.getOwnPropertyDescriptor(this.attributes,attr[i]).value; //extrae los valores de cada propiedad para sacar sus correspondientes componentes
+            if(elem.id==id){
+                return [attr[i],elem];
+            }
+            
+        }
+        // for(let i=0;i<attr.length;i++){
+        //     if(this.attributes.attr[i].id==id){
+        //         console.log("aqui");
+        //         obj = Object.getOwnPropertyDescriptor(this.attributes.attr[i]);
+        //     }
+        // }
+        console.log(obj);
     }
 }
 
@@ -39,7 +61,7 @@ class Vampire extends Character {
                 weak: "",
                 primal_disc: new Array(3)
             };
-        this.identity.gen = 0;
+        this.identity.gen = 14;
         this.virtues = {
             vir_1: {
                 type: "",
@@ -65,15 +87,65 @@ class Vampire extends Character {
     }
 }
 
+
 initializeSheet = ()=>{
-    let writer = "";    
+    let writer = "";
+    vamp1=new Vampire();
+    writer = createAttributes(vamp1);
+    mainAttr.innerHTML = writer;
+}
+
+createAttributes=(char)=>{
+    let writer = "";
+    let attribute = ["Fuerza","Carisma","Percepción","Destreza","Manipulación","Inteligencia","Resistencia","Apariencia","Astucia"];
+    let count = 0;
+    let maxAttr = vampGenCalculator(char.identity.gen);
+    for(let i=0; i<3; i++){
+        writer+='<div class=row justify-content-center align-items-center g-2`>'
+        for(let j=0; j<3; j++){
+            writer+=`<div class="col-4">${attribute[count]} <div class="d-inline">(_____)</div>`;
+            let obj = char.getAttrByID(count);
+            let name = obj[0];
+            console.log(name);
+            for(let k = 0;k<=maxAttr;k++){
+                writer+=`<input class="stat" type="checkbox" id="${name}${k+1}" name="${name}_dot" value="1" onchange="levelSelect('${name}_dot',${k})"`
+                if(k==0){
+                    writer+=`checked>`
+                }else{
+                    writer+=`>`
+                }
+            }
+            writer+=`</div>`;
+            count++;
+        }
+        writer+=`</div>`
+    }
+    return writer;
+}
+
+// Generacion:
+// 0 = humano // no tiene
+// 1 - 3 = caín y antediluvianos, stats ilimitados
+// 4 - 7 = disminuyen en 1 mientras aumenta la generacion
+// 8 o + = 5
+
+vampGenCalculator=(gen)=>{
+    if(gen<4){
+        return 10;
+    }
+    else if(gen<8){
+        let maxAttr = 13-gen;
+        return maxAttr;
+    }else{
+        return 5;
+    }
 }
 
 levelSelect = (name,pos) =>{
     let elem = document.getElementsByName(name);
-    let valor;
+    let value;
     if(pos==0){
-        valor = firstPosition(name);
+        value = firstPosition(name);
     }
     else{
         if(elem[pos].checked){
@@ -90,9 +162,9 @@ levelSelect = (name,pos) =>{
             elem[pos].checked = true;
             levelSelect(name,pos);
         }
-        valor = parseInt(elem[pos].value);
+        value = parseInt(elem[pos].value);
     }
-    console.log(valor);
+    console.log(value);
 }
 
 firstPosition=(name) => {
